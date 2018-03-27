a24445ecca6bb0bcc625dcc15ac7c33027c81004
Chris <chris.anthony.yee@gmail.com>
2018-03-26 21:28:31 -0700

    incoming email

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'incoming/create'
+
   resources :topics do
     resources :bookmarks
   end
-  
+
   devise_for :users
   resources :users
 
-
+  post :incoming, to: 'incoming#create'
+  
   get 'about' =>'welcome#about'
 
   root 'welcome#index'
